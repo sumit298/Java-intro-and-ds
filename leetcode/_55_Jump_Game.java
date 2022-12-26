0e4ca10749210a8df7b03c4c970bc1987d6055a4
sumit298 <sumitsinhassss@outlook.com>
2022-12-26 21:19:04 +0530

    jump game leetcode dec 26

---
+++ leetcode/_55_Jump_Game.java
@@ -0,0 +1,29 @@
+public class _55_Jump_Game {
+    public static boolean canJump(int nums[]){
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i > max){
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
+        }
+        return true;
+    }
+
+    // Alisha approach
+    public static boolean canJump2(int nums[]){
+        // Working from the last index to the first index.
+        int index = nums.length-1;
+        for(int i=nums.length-1; i>=0; i--){
+            if(nums[i] + i > index){
+                index = i;
+            }
+        }
+        if(index == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
